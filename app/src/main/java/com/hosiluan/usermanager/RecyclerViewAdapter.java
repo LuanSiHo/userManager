9874e081b16ccfdb1076f4826c3a176e6f17d9fd
unknown <luanho192@gmail.com>
2017-10-30 17:21:17 +0700

    update at company on 30/10/2017

--- app/src/main/java/com/hosiluan/usermanager/RecyclerViewAdapter.java
+++ app/src/main/java/com/hosiluan/usermanager/RecyclerViewAdapter.java
@@ -1,13 +1,24 @@
 package com.hosiluan.usermanager;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.IntentCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.chauthai.swipereveallayout.SwipeRevealLayout;
+import com.chauthai.swipereveallayout.ViewBinderHelper;
 
 import java.util.ArrayList;
 
@@ -19,23 +30,34 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     private Context mContext;
     private ArrayList<User> mUserList;
+    private AdapterListener adapterListener;
 
-    public RecyclerViewAdapter(Context mContext, ArrayList<User> mUserList) {
+
+    public RecyclerViewAdapter(Context mContext, ArrayList<User> mUserList,
+                               AdapterListener adapterListener) {
         this.mContext = mContext;
         this.mUserList = mUserList;
+        this.adapterListener = adapterListener;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-        View view = LayoutInflater.from(mContext).inflate(R.layout.user_item,parent,false);
+        View view = LayoutInflater.from(mContext).inflate(R.layout.user_item, parent, false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        User user = mUserList.get(position);
+        final User user = mUserList.get(position);
+
         holder.usernameTextView.setText(user.getmLastName() + " " + user.getmFirstName());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                adapterListener.onItemClickEvent(user);
+            }
+        });
     }
 
     @Override
@@ -43,16 +65,20 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         return mUserList.size();
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder{
+    class ViewHolder extends RecyclerView.ViewHolder {
 
-        TextView usernameTextView;
+        TextView usernameTextView, deleteTextView;
         RelativeLayout viewBackground, viewForeground;
 
         public ViewHolder(View itemView) {
             super(itemView);
             usernameTextView = itemView.findViewById(R.id.tv_user_name);
-            viewBackground = itemView.findViewById(R.id.view_background);
             viewForeground = itemView.findViewById(R.id.view_foreground);
+
         }
+    }
+
+    interface AdapterListener{
+        void onItemClickEvent(User user);
     }
 }
---
+++ app/src/main/java/com/hosiluan/usermanager/fragment/UserInfoToolbarFragment.java
@@ -0,0 +1,80 @@
+package com.hosiluan.usermanager.fragment;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.hosiluan.usermanager.R;
+
+/**
+ * Created by User on 10/30/2017.
+ */
+
+public class UserInfoToolbarFragment extends Fragment {
+
+    private TextView editUserInfoTextView;
+    private ImageButton backImageButton;
+    private UserInfoFragmentListener userInfoFragmentListener;
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_user_info_toolbar,container,false);
+        editUserInfoTextView = view.findViewById(R.id.tv_edit_user_info);
+        backImageButton = view.findViewById(R.id.img_btn_user_info_back);
+        return view;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        userInfoFragmentListener = (UserInfoFragmentListener) getActivity();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        setEvent();
+    }
+
+    private void setEvent(){
+        editUserInfoTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity().getApplicationContext(), "edittt", Toast.LENGTH_SHORT).show();
+
+                EditUserInfoFragment editUserInfoFragment = new EditUserInfoFragment();
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container,editUserInfoFragment);
+                fragmentTransaction.commit();
+                userInfoFragmentListener.onEditUserInfoEvent();
+            }
+        });
+        backImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity().getApplicationContext(), "back", Toast.LENGTH_SHORT).show();
+                userInfoFragmentListener.onBackToUserListEvent();
+            }
+        });
+    }
+
+
+    public interface UserInfoFragmentListener {
+        void onBackToUserListEvent();
+        void onEditUserInfoEvent();
+    }
+}
